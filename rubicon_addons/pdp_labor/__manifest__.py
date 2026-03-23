75f81fd4f9981b3e196938cdbb93c870e3fb211b
Blopausore <augustin_650@hotmail.fr>
2026-03-23 19:22:24 +0100

    feat(pdp_labor): auto-load labor and addon types on install

--- rubicon_addons/pdp_labor/__manifest__.py
+++ rubicon_addons/pdp_labor/__manifest__.py
@@ -5,6 +5,9 @@
     'depends': ['rubicon_env', 'pdp_product'],
     'data': [
         './security/ir.model.access.csv',
+        # Data
+        'data/pdp.labor.type.csv',
+        'data/pdp.addon.type.csv',
         # Views
         'views/pdp_views.xml',
         'views/pdp_menus.xml',
