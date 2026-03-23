2fba61750a30b7947fce694df644ad8008d941d3
Blopausore <augustin_650@hotmail.fr>
2026-03-23 13:00:22 +0100

    feat(rubicon_demo): add module skeleton with post_init_hook

---
+++ rubicon_addons/rubicon_demo/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Rubicon Demo Data',
+    'version': '0.1.0',
+    'license': 'LGPL-3',
+    'depends': [
+        'pdp_frontend',
+        'sis_frontend',
+        'rubicon_uom',
+        'metal_price',
+    ],
+    'data': ['data/rubicon_demo_data.xml'],
+    'post_init_hook': 'post_init_hook',
+    'installable': True,
+    'application': False,
+}
